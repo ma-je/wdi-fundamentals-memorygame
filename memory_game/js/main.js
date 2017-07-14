8d0e0a1dbd42a612fd4d99158034c7a3327e2ae9
ma-je <mmonteiro401@gmail.com>
2017-07-14 15:49:33 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("up and running!");
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+
+console.log("User flipped " +  cardOne);
+console.log("User flipped "  +  cardFour);
