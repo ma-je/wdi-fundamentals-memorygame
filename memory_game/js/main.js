72a8cbfddd65aaa68a4bb6017a88aa690d8dea9a
ma-je <mmonteiro401@gmail.com>
2017-07-17 09:00:05 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,20 +4,73 @@ var cardFour = "queen";
 console.log("User flipped " +  cardOne);
 console.log("User flipped "  +  cardFour);
 */
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: 'queen',
+  suit: 'hearts',
+  cardImage: 'images/queen-of-hearts.png',
+},
+{
+  rank: 'queen',
+  suit: 'diamonds',
+  cardImage: 'images/queen-of-diamonds.png',
+},
+{
+  rank: 'king',
+  suit: 'hearts',
+  cardImage: 'images/king-of-hearts.png',
+},
+{
+  rank: 'king',
+  suit: 'diamonds',
+  cardImage: "images/king-of-diamonds.png",
+}
+];
+
 var cardsInPlay =[];
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-console.log("User flipped " + cardOne);
+//cardsInPlay.push(cards[cardId]);
 
-var cardTwo = cards[3];
-cardsInPlay.push("cardTwo");
-console.log("User flipped " + cardTwo);
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]){
+      alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+};
 
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cardId);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
+  this.setAttribute('src', cards[cardId].cardImage);
+  if (cardsInPlay.length===2) {
+    checkForMatch();
+  }
 
-if (cardsInPlay[0] === cardsInPlay[3]) {
-  alert("You found a match!");
-}
-else {
-  alert("Sorry, try again.");
-}
+};
+
+var gameBoard = document.getElementById('game-board');
+
+  var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var newCard = document.createElement('img');
+    newCard.setAttribute('src', "images/back.png");
+    newCard.setAttribute('data-id', i);
+    newCard.addEventListener('click', flipCard);
+    gameBoard.appendChild(newCard);
+  }
+};
+createBoard();
+
+  /*if (cardsInPlay[0] === cardsInPlay[3]) {
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again.");
+  }
+
+  where is this supposed to go Next update the cards[cardId] portion of cardsInPlay.push(cards[cardId]);. You'll want to push the name of the card ("queen" or "king") to the cardsInPlay array.
+*/
